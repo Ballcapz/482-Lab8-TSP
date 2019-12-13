95d1491402149d6cb5ce1202e279897b07ef0cf0
Zach Johnson <zlg23johnson@gmail.com>
2019-12-13 15:53:01 -0600

    Updates to running the algorithms

--- Runner.java
+++ Runner.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Random;
 
 import genome.GeneticTSP;
@@ -14,15 +13,26 @@ class Runner {
 
         int n = 10;
         int[][] matrix = new int[n][n];
-        for (int[] row : matrix)
-            java.util.Arrays.fill(row, 100);
+        // for (int[] row : matrix)
+        // java.util.Arrays.fill(row, 100);
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (i == j) {
+                    matrix[i][j] = 0;
+                } else {
+                    int temp = rand.nextInt(50);
+                    matrix[i][j] = temp;
+                    matrix[j][i] = temp;
+                }
+            }
+        }
 
-        // Make this be random for a better check
-        // Construct an optimal tour
-        int edgeCost = 5;
-        int[] optimalTour = { 2, 7, 6, 1, 9, 8, 5, 3, 4, 0, 2 };
-        for (int i = 1; i < optimalTour.length; i++)
-            matrix[optimalTour[i - 1]][optimalTour[i]] = edgeCost;
+        // // Make this be random for a better check
+        // // Construct an optimal tour
+        // int edgeCost = 5;
+        // int[] optimalTour = { 2, 7, 6, 1, 9, 8, 5, 3, 4, 0, 2 };
+        // for (int i = 1; i < optimalTour.length; i++)
+        // matrix[optimalTour[i - 1]][optimalTour[i]] = edgeCost;
 
         System.out.println("Normal tsp...");
         int[] bestTour = tsp.bruteForce(matrix);
@@ -57,24 +67,26 @@ class Runner {
 
         // GENETIC ********************************************************
 
-        int[][] geneticMatrix = new int[n][n];
+        // int[][] geneticMatrix = new int[n][n];
 
-        // fill up the genetic matrix with values up to random
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j <= i; j++) {
-                if (i == j) {
-                    geneticMatrix[i][j] = 0;
-                } else {
-                    int temp = rand.nextInt(50);
-                    geneticMatrix[i][j] = temp;
-                    geneticMatrix[j][i] = temp;
-                }
-            }
-        }
+        // // fill up the genetic matrix with values up to random
+        // for (int i = 1; i < n; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // if (i == j) {
+        // geneticMatrix[i][j] = 0;
+        // } else {
+        // int temp = rand.nextInt(50);
+        // geneticMatrix[i][j] = temp;
+        // geneticMatrix[j][i] = temp;
+        // }
+        // }
+        // }
 
-        GeneticTSP geneticTSP = new GeneticTSP(n, geneticMatrix, 0, 0);
+        GeneticTSP geneticTSP = new GeneticTSP(n, matrix, 0, 0);
         Salesman genResult = geneticTSP.optimize();
         System.out.println("Genetic result...");
         System.out.println(genResult);
+        System.out.println();
+
     }
 }
